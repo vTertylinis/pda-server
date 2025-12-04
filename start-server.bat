98308871dd97f712ab0c3077b8918096c50ef426
vTertylinis <v.tertylinis@gmail.com>
2025-12-04 14:29:00 +0200

    ff

--- start-server.bat
+++ start-server.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "C:\PersonalTest\restaurant-backend"
-start "Node Server" node server.js
+start /MIN "Node Server" node server.js
 
 rem Launch ngrok from the script's directory on port 4300
 rem Keep window open to show ngrok output/errors
-start "ngrok" cmd /k "%~dp0ngrok.exe" http 4300
+start /MIN "ngrok" cmd /k "%~dp0ngrok.exe" http 4300
